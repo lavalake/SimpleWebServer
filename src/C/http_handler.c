f456997d4fee2e3887e8b0152770b279c918ec92
jason <lavalakewl@gmail.com>
2015-02-24 14:07:41 -0500

    add send response body

--- src/C/http_handler.c
+++ src/C/http_handler.c
@@ -11,6 +11,7 @@
 #include <signal.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 /* Includes related to socket programming */
 #include <netinet/in.h>
 #include <netinet/ip.h>
@@ -20,9 +21,10 @@
 #include "http_handler.h"
 #define URLLEN 200 //max URL length
 #define RSP_HEADER_LEN 65
-/*
-static char default_page[] = "../reference/www/index.html";
+#define BUF_SIZE 1024
+static char default_page[] = "index.html";
 #define PATH "../reference/www/"
+/*
 static char error_bad[] = {"HTTP/1.0 400 Bad Request\r\n"
     "Content-type: text/html\r\n\r\n"
     "<html><body>Error</body></html>\r\n\r\n"};
@@ -36,13 +38,22 @@ static char content_type[] = "Content-type: ";
 void parseRequest(int fd,char *request){
 
     HTTPRSP rsp;
+    char path[]=PATH;
+    int path_len=0;
     rsp.file_name = (char*)malloc(URLLEN);
+    strcpy(rsp.file_name,path);
+    path_len = strlen(path);
+    strcpy(rsp.file_name+path_len,default_page);
     sendRsp(fd,rsp);
 }
 void sendRsp(int fd,HTTPRSP rsp){
     char header[RSP_HEADER_LEN];
+    char file_buf[BUF_SIZE];
+    int total_len=0,read_length=0;
     int total=0;
     int total_sent=0,bytes_sent=0;
+    int read_fd;
+    struct stat stat_buf;
     strcpy(header,ok_rsp);
     total = strlen(header);
     strcpy(header+total,server_info);
@@ -59,6 +70,23 @@ void sendRsp(int fd,HTTPRSP rsp){
             } else {
                 total_sent += bytes_sent;
             }
+    }
+    printf("file name %s\n",rsp.file_name);
+    read_fd = open(rsp.file_name,O_RDONLY);
+    fstat(read_fd,&stat_buf);
+    while(total_len < stat_buf.st_size){
+       int total_send = 0;
+       read_length = read(read_fd,file_buf,BUF_SIZE);
+       total_len += read_length;
+       while(total_send < read_length){
+           int send_len = 0;
+           send_len = send(fd,file_buf+total_send,read_length-total_send,0);
+           if(send_len < 0){
+               break;
+           }else{
+               total_send += send_len;
+           }
+       }
     }
 
 }
